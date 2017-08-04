49cab216b96686c4df3d6fe3db76094e2cb7bcf8
Talitha Pumar <tapumar@gmail.com>
2017-08-04 15:33:42 -0300

    Adding some tests

--- ChatBot/testing.py
+++ ChatBot/testing.py
@@ -45,9 +45,21 @@ class TestTalks(unittest.TestCase):
         self.assertEqual(conversa('Não, e você?'), 'Estou bem, mas o que houve contigo? Quer conversar sobre isso?')
         self.assertEqual(conversa('Não'), 'O que houve? Quer conversar sobre isso?')
 
-    # def test_brincar(self):
-    #     self.assertEqual(conversa('O que você sabe fazer?'), 'Muitas coisas! Conversar, jogar, etc.')
-    #     self.assertEqual(conversa('Que jogos?'), 'Adivinhar o número, quer jogar?')
+    def test_brincar(self):
+        self.assertEqual(conversa('O que você sabe fazer?'), 'Muitas coisas! Conversar, jogar, etc.')
+        self.assertEqual(conversa('Que jogos?'), 'Adivinhar o número, quer jogar?')
+        self.assertEqual(conversa('Sim!'), 'Adivinhe um número entre 1 e 100')
+
+    def test_chat_negativo(self):
+        self.assertEqual(conversa('Ando tão triste ultimamente'), 'O que houve? Quer conversar?')
+        self.assertEqual(conversa('Não estou conseguindo estudar direito :('), 'Poxa :( Você sabe o motivo de você não conseguir estudar direito?')
+        self.assertEqual(conversa('Não...'), 'Você quer ajuda? Posso te indicar umas ferramentas...')
+        self.assertEqual(conversa('Quais ferramentas vc me indica?'), 'Já ouviu falar no trello? Ou Wunderlist?')
+
+    def test_chat_positivo(self):
+        self.assertEqual(conversa('Meu dia foi muito bom'),'Que bom! Me conte como foi')
+        self.assertEqual(conversa('Estou feliz'),'Maravilha! Compartilhe essa felicidade comigo')
+        self.assertEqual(conversa(''),'')
 
 
     # def test_iniciador_de_conversas(self):
